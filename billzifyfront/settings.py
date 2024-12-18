9c562b09f7c6e39ab791d98846473a50b7b4c51a
Billzify1013 <ckhajwan@gmail.com>
2024-12-18 19:18:05 +0530

    new update

--- billzifyfront/settings.py
+++ billzifyfront/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'billzifyfront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
